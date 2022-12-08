4d23a7fe38b6ef2154afb087bb79d0dcdbccba4d
임수연 <96305365+printyeon@users.noreply.github.com>
2022-12-08 11:35:47 +0900

    초기 커밋.

---
+++ gameMain.cpp
@@ -0,0 +1,629 @@
+#include<SFML/Graphics.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <string.h>
+#include <windows.h>
+
+#define WIDTH 920
+#define HEIGHT 920
+
+using namespace std;
+using namespace sf;
+
+struct Position
+{
+	int x;
+	int y;
+};
+
+RenderWindow window(VideoMode(WIDTH, HEIGHT), "jump woong");
+
+//화면 전환
+bool mainScene = true;
+bool gameScene = false;
+
+int index = 1;    //왼발 오른발 인덱스
+int floatBottom = -300; //바닥에서 좀 띄우는 용도
+int PRICKLE_Y_BOTTOM = HEIGHT + floatBottom + 20;    //나무 바닥
+int hp = 30;
+int prickleSpeed = 6;    //나무 스피드
+float gravity = 4.3f;    //중력. 점프할때 사용
+bool stop = false;//걸을건지 안걸을 건지
+bool isreach = false; //
+
+bool isCollide(Sprite s1, Sprite s2)
+{
+	return s1.getGlobalBounds().intersects(s2.getGlobalBounds());
+}
+
+int textPrint(Text& textMsg, Font& font, int size,
+	float x, float y, const Color& color, string p)
+{
+	textMsg.setFont(font);             //폰트
+	textMsg.setCharacterSize(size);    //크기 
+	textMsg.setPosition(x, y);         //x, y 위치
+	textMsg.setFillColor(color);       //색
+	textMsg.setString(p); //출력할 문자열
+
+	return 0;
+
+};
+
+class Player {
+private:
+
+	Texture t1;
+	Texture t2;
+
+	int DINO_Y_BOTTOM = HEIGHT + floatBottom;
+	Position dinoPos;
+
+	//walk
+	float frame = 0.f;
+	float frameSpeed = 0.4f;
+	const int changeCount = 5;    //몇 프레임에 체인지 할지
+
+	//jump
+	bool isJumping = false;    //점프 중인지
+	bool isBottom = true;    //바닥에 발이 닿았는지
+
+	//뛰는 소리
+	SoundBuffer buffer1;
+
+public:
+
+	//getter
+	Position getDinoPos() const {
+		return dinoPos;
+	}
+
+	Sprite dinoInit() {
+
+		t1.loadFromFile("img/dino1.png");
+		Sprite dino(t1);
+
+		return dino;
+	}
+
+	Sprite dino2Init() {
+
+		t2.loadFromFile("img/dino2.png");
+		Sprite dino2(t2);
+
+		return dino2;
+	}
+
+	//생성과 동시에 초기화
+	Player() {
+
+		dinoPos.x = 30;
+		dinoPos.y = DINO_Y_BOTTOM;
+
+	}
+
+	void jumpCheck() {
+		buffer1.loadFromFile("sound/jumpS.wav");
+		Sound jumpS(buffer1);
+
+		if (isBottom && !isJumping)    //바닥이고, 점프중이 아닐때
+		{
+			jumpS.play();
+
+			isJumping = true;
+			isBottom = false;
+
+		}
+	}
+
+	void jump() {
+
+		if (isJumping)
+		{
+			dinoPos.y -= gravity; //점프중일때는 y에서 중력을 뺴줌 그럼 올라감
+
+			stop = true;
+		}
+		else
+		{
+			dinoPos.y += gravity * 1.5;
+			stop = false;
+		}
+
+		//극한
+		if (dinoPos.y >= DINO_Y_BOTTOM)    //바닥에서 지하로 가지 않도록 설정
+		{
+			dinoPos.y = DINO_Y_BOTTOM;
+
+			isBottom = true;
+
+		}
+		if (dinoPos.y <= DINO_Y_BOTTOM - 250)    //점프해서 우주로 가지 않도록 설정
+		{
+			isJumping = false;
+		}
+		if(isBottom)
+			isreach = false;
+	}
+
+	void walk() {
+		if (stop) {
+			index = 0; //인덱스 고정시켜서 멈춰있기
+		}
+		else {
+			//dino step.
+			frame += frameSpeed;
+			if (frame > changeCount)
+			{
+				frame -= changeCount;
+				++index;
+				
+				if (index == 2) {
+					index = 0;
+				}
+			}
+		}
+	}
+
+	void reset() {
+		dinoPos.y = DINO_Y_BOTTOM;
+		bool isJumping = false;
+	}
+
+};
+
+class Coin {
+private:
+	Texture t6;
+	Position coinPos;
+
+public:
+
+	//getter
+	Position getCoinPos() const {
+		return coinPos;
+	}
+
+	Sprite coinInit() {
+		t6.loadFromFile("img/coin_01.png");
+		Sprite coin(t6);
+
+		return coin;
+	}
+
+	Coin() {
+
+		coinPos.x = WIDTH - 20; //화면 밖에서 먼저 나오게 하기
+		coinPos.y = PRICKLE_Y_BOTTOM;
+
+	}
+
+	void move() {
+		int randy = rand() % 200 + 470;
+		int randx = rand() % 300 + 920;
+
+		// 생성
+		if (coinPos.x <= -30)
+		{
+			coinPos.x = randx;
+			coinPos.y = randy;
+
+		}
+		else
+		{
+			coinPos.x -= prickleSpeed;
+		}
+	}
+
+	void trans() {
+		coinPos.x = -200;
+		coinPos.y = -200;
+	}
+
+	void reset() {
+		coinPos.x = WIDTH - 20;
+	}
+};
+
+class Prickles {
+private:
+
+	//prickle
+	Texture t3;
+	//prickle 2
+	Texture t7;
+
+	Position pricklePos;
+	Position prickle2Pos;
+
+
+	int prickleCnt = 0;
+
+
+public:
+
+	//getter
+	Position getPricklePos() const {
+		return pricklePos;
+	}
+
+	Position getPrickle2Pos() const {
+		return prickle2Pos;
+	}
+
+
+	Sprite prickleInit() {
+		t3.loadFromFile("img/tree.png");
+		Sprite prickle(t3);
+
+		return prickle;
+	}
+
+	Sprite prickle2Init() {
+
+		t7.loadFromFile("img/prickle.png");
+		Sprite prickle2(t7);
+
+		return prickle2;
+	}
+
+
+	Prickles() {
+		
+		pricklePos.x = WIDTH - 20; //화면 밖에서 먼저 나오게 하기
+		pricklePos.y = PRICKLE_Y_BOTTOM;
+		
+		prickle2Pos.x = WIDTH - 20; //화면 밖에서 먼저 나오게 하기
+		prickle2Pos.y = PRICKLE_Y_BOTTOM;
+	
+	}
+	void move() {
+
+		//장애물 5개를 넘을때 마다 랜덤으로 가시 스피드 조절
+		if (prickleCnt == 5) {
+			prickleSpeed = rand() % 10 + 6;
+			gravity = 4.3f + (prickleSpeed / 4);
+			prickleCnt = 0;
+		}
+
+		// 가시 생성
+		if (prickle2Pos.x <= 0 && pricklePos.x <= 0)
+		{
+			pricklePos.x = 1000;
+			prickleCnt++;
+		}
+		else
+		{
+			pricklePos.x -= prickleSpeed;
+		}
+
+		//가시 생성
+		if (prickle2Pos.x <= 0 && pricklePos.x <= 0)
+		{
+			prickle2Pos.x = 1000;
+			prickleCnt++;
+		}
+		else
+		{
+			prickle2Pos.x -= prickleSpeed;
+		}
+	}
+
+	void reset() {
+		prickleCnt = 0;
+		prickle2Pos.x = WIDTH - 20;
+		pricklePos.x = WIDTH - 20;
+	}
+};
+
+int main(void)
+{
+	window.setFramerateLimit(60);    //프레임
+	static int score = -1; //0
+
+#pragma region 소리
+
+		//메인 소리
+	SoundBuffer buffer;
+	buffer.loadFromFile("sound/mainS.wav");
+	Sound mainS(buffer);
+
+
+
+	//동전 먹는 소리
+	SoundBuffer buffer2;
+	buffer2.loadFromFile("sound/coinS.wav");
+	Sound coinS(buffer2);
+
+#pragma endregion
+
+
+#pragma region 텍스트
+
+	Text text1;
+	Text text2;
+	Text text3;
+	Text text8;
+	Text text4;
+	Text text6;
+	Text text7;
+
+	string scoretxt = to_string(score);
+	int x = 0, y = 0;
+
+#pragma endregion
+
+
+#pragma region 배경
+
+	//main bg
+	Texture t4;
+	t4.loadFromFile("img/main.png");
+	Sprite Main(t4);
+
+	//game bg
+	Texture t5;
+	t5.loadFromFile("img/game.png");
+	Sprite Game(t5);
+
+#pragma endregion
+
+
+	/////////////cloud
+	Texture t8;
+	t8.loadFromFile("img/cloud.png");
+	Sprite cloud(t8);
+
+	Position cloudPos;
+	cloudPos.x = WIDTH - 20; //화면 밖에서 먼저 나오게 하기
+	cloudPos.y = 30;
+
+	/////////////grass
+	Texture t9;
+	t9.loadFromFile("img/grass.png");
+	Sprite grass(t9);
+
+	Position grassPos;
+	grassPos.x = WIDTH - 20; //화면 밖에서 먼저 나오게 하기
+	grassPos.y = 0;
+
+	/////////////이어짐을 위한 grass2
+	Texture t10;
+	t10.loadFromFile("img/grass.png");
+	Sprite grass2(t10);
+
+	Position grass2Pos;
+	grass2Pos.x = WIDTH - 20 - 1000; //화면 밖에서 먼저 나오게 하기
+	grass2Pos.y = 0;
+
+	//hp bar
+	RectangleShape rect(Vector2f(hp*15, 30));
+	rect.setPosition(95, 12);
+
+	Player p = Player();
+	Prickles pri = Prickles();
+	Coin c = Coin();
+
+	Sprite prickle = pri.prickleInit();
+	Sprite prickle2 = pri.prickle2Init();
+
+	Sprite coin = c.coinInit();
+
+	Sprite dinoArr[2];
+	dinoArr[0] = p.dinoInit();
+	dinoArr[1] = p.dino2Init();
+
+	Font font;
+	font.loadFromFile("font/Starborn.ttf");
+	
+	mainS.play();
+
+	while (window.isOpen())
+	{
+		Event e;
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		//메인 화면
+		if (mainScene)
+		{
+
+			textPrint(text6, font, 40, 270, 810, Color::White, "Enter To Start");
+			textPrint(text7, font, 80, 130, 210, Color::White,"jump woong");
+
+			if (Keyboard::isKeyPressed(Keyboard::Enter)) //엔터 입력 감지
+			{
+				mainScene = false;
+				gameScene = true;
+			}
+
+			window.clear(Color::White);
+			window.draw(Main);
+			window.draw(text6);
+			window.draw(text7);
+
+		}
+
+		//인게임 화면
+		if (gameScene) {
+
+			//score
+			textPrint(text2, font, 30, 680, 10, Color::White, "Score : ");
+			textPrint(text1, font, 30, 840, 12, Color::White, scoretxt);
+
+			//hp
+			textPrint(text4, font, 30, 30, 10, Color::White, "Hp ");
+
+
+#pragma region 게임 오버
+
+			if (hp == 0) {
+				prickleSpeed = 0;
+				textPrint(text3, font, 80, 150, 210, Color::White, "Game Over");
+				textPrint(text8, font, 40, 190, 330, Color::White, "press M to main");
+				stop = true; //가만히 있게 하기
+
+				if (Keyboard::isKeyPressed(Keyboard::M)) //스페이스 입력 감지
+				{
+					mainScene = true;
+					gameScene = false;
+					hp = 30;
+					rect.setSize(Vector2f(hp * 15, 30));
+					prickleSpeed = 6;
+					score = 0;
+					scoretxt = to_string(score);
+					stop = false;
+					
+					//텍스트 잠시 안보이게 다른곳으로 옮겨 놓기
+					text8.setPosition(-100, -100);
+					text3.setPosition(-100, -100);
+
+					//위치 리셋
+					pri.reset();
+					p.reset();
+					c.reset();
+				}
+			}
+			else {
+#pragma region 충돌처리
+				
+					if (isCollide(dinoArr[0], prickle) || isCollide(dinoArr[1], prickle) || isCollide(dinoArr[0], prickle2) || isCollide(dinoArr[1], prickle2))
+					{
+						//if(p.getDinoPos().y > 500)
+							//printf("sdfafasf");
+
+						
+						if (hp == 0) {
+							hp = 0; //마이너스로 안 가도록
+						}
+						else {
+							if (!isreach) {
+									hp--;
+									isreach = true;
+									rect.setSize(Vector2f(hp * 15, 30));
+								
+						}
+						
+					}
+				}
+
+				if (isCollide(dinoArr[0], coin) || isCollide(dinoArr[1], coin)) {
+					coinS.play();
+
+					c.trans(); //안보이게 숨기기
+					score++;
+					scoretxt = to_string(score);
+				}
+#pragma endregion
+
+				//점프
+				if (Keyboard::isKeyPressed(Keyboard::Space)) //스페이스 입력 감지
+				{
+					p.jumpCheck();
+				}
+				p.jump();
+
+				//걷기
+				p.walk();
+
+
+#pragma region 부가적인것들
+
+
+				// 구름 생성
+				if (cloudPos.x <= -900)
+				{
+					cloudPos.x = WIDTH;
+
+				}
+				else
+				{
+					cloudPos.x -= prickleSpeed;
+				}
+
+				// 잔디 생성
+				if (grassPos.x <= -1200)
+				{
+					grassPos.x = WIDTH;
+
+				}
+				else
+				{
+					grassPos.x -= prickleSpeed;
+				}
+
+				// 잔디2 생성
+				if (grass2Pos.x <= -1200)
+				{
+					grass2Pos.x = WIDTH;
+
+				}
+				else
+				{
+					grass2Pos.x -= prickleSpeed;
+				}
+#pragma endregion
+
+				//움직일 수 있도록
+				pri.move();
+				c.move();
+			}
+
+
+
+
+			//pricklePos
+			prickle.setPosition(pri.getPricklePos().x, pri.getPricklePos().y);
+			prickle2.setPosition(pri.getPrickle2Pos().x, pri.getPrickle2Pos().y);
+
+			//coin Pos
+			coin.setPosition(c.getCoinPos().x, c.getCoinPos().y);
+
+			//cloud Position.
+			cloud.setPosition(cloudPos.x, cloudPos.y);
+
+			//grass Position.
+			grass.setPosition(grassPos.x, grassPos.y);
+			grass2.setPosition(grass2Pos.x, grass2Pos.y);
+
+			//dino Pos
+			dinoArr[index].setPosition(p.getDinoPos().x, p.getDinoPos().y);
+
+			//draw.
+			window.clear(Color::White);
+			window.draw(Game);
+
+			window.draw(cloud);
+			window.draw(grass);
+			window.draw(grass2);
+			window.draw(text1);
+			window.draw(text2);
+			window.draw(text4);
+			window.draw(text8);
+			window.draw(text3);
+
+			window.draw(coin);
+			window.draw(prickle);
+			window.draw(prickle2);
+			window.draw(dinoArr[index]);
+			window.draw(rect);
+
+
+
+
+		}
+
+
+		window.display();
+	}
+
+	return 0;
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,185 @@
+﻿#include <SFML/Graphics.hpp>
+#include <conio.h>
+#include <iostream>
+#include <Windows.h>
+#define WIDTH 1920
+#define HEIGHT 1080
+//https://blockdmask.tistory.com/371
+using namespace sf;
+struct Postition
+{
+	int x;
+	int y;
+};
+
+int main(void)
+{
+	RenderWindow app(VideoMode(1920, 1080), "Dino Game");
+	app.setFramerateLimit(60); //60프레임
+
+		
+		Texture t1;
+		t1.loadFromFile("img/Walk (4).png");
+		//Texture t2;
+		//t2.loadFromFile("img/Walk (1).png");
+		Texture t3;
+		t3.loadFromFile("img/Walk (9).png");
+
+		Sprite dinoArr[2];
+		dinoArr[0] = Sprite(t1);
+		dinoArr[1] = Sprite(t3);
+
+		//공룡 바닥 위치
+		static const int DINO_Y_BOTTOM = HEIGHT - t1.getSize().y - 80;
+	
+
+
+		Postition dinoPos;
+		dinoPos.x  = 200;
+		dinoPos.y  = DINO_Y_BOTTOM;
+
+		//프레임에 따라 왼발 오른발 해주기 위한 변수 네개
+		int index = 0;
+		//왼발 오른발 인덱스    
+		float frame = 0.f;
+		
+		float frameSpeed = 0.4f;
+		const int changeCount = 5; //몇 프레임에 체인지 할지
+
+		const int gravity = 2;//중력. 점프할때 사용    
+		bool isJumping = false;//점프 중인지   
+		bool isBottom = true; //바닥에 발이 닿았는지
+
+		Texture t4;
+		t4.loadFromFile("img/spike B.png");
+
+		Sprite spike(t4);
+		
+		static const int SPIKE_Y_BOTTOM = HEIGHT - t4.getSize().y - 80;
+
+		Postition spPos;
+		spPos.x = WIDTH - 20;
+		spPos.y = SPIKE_Y_BOTTOM ;
+		const int spkieSpeed = 4;
+
+		//윈도우 닫기
+		while(app.isOpen())
+		{
+			Event e;
+			while (app.pollEvent(e))
+			{
+				if (e.type == Event::Closed)
+					app.close();
+			}
+		
+
+
+		if(Keyboard::isKeyPressed(Keyboard::Space)) //스페이스 입력 감지
+		{
+
+			if (isBottom && !isJumping)
+			{
+				//printf("asdfasdfafsd");
+				isJumping = true;
+				isBottom = false;
+			}
+
+			
+
+			/*
+			//printf("asdfasdfafsd");
+			
+			*/
+		}
+		if (isJumping)
+		{
+			//dinoPos.y += gravity;
+			dinoPos.y -= gravity; //점프중일때는 y에서 중력을 뺴줌 그럼 올라감
+		}
+
+		//애니메이션
+		frame += frameSpeed;
+		if (frame > changeCount)
+		{
+			frame -= changeCount;
+			++index;
+			if (index >= 2) { index = 0; }
+		}
+
+		//dino Position.        
+		dinoArr[index].setPosition(dinoPos.x, dinoPos.y);
+
+		
+
+
+
+		/*
+		if (isJumping)
+		{
+			//dinoPos.y += gravity;
+			dinoPos.y -= gravity; //점프중일때는 y에서 중력을 뺴줌 그럼 올라감
+		}
+		else {
+			dinoPos.y += gravity;
+		}
+
+		if (dinoPos.y = DINO_Y_BOTTOM)
+		{
+			dinoPos.y = DINO_Y_BOTTOM;
+			isBottom = true;
+		}
+
+
+		if (dinoPos.y <= DINO_Y_BOTTOM - 100)
+		{
+			isJumping = false;
+		}
+
+
+		
+		
+
+
+		
+
+		
+
+		//tree move.        
+		if(spPos.x <= 0)
+		{
+			spPos.x = WIDTH;
+		}
+		else
+		{
+			spPos.x -= spkieSpeed;
+		}
+		
+		//spike Pos.        
+		spike.setPosition(spPos.x, spPos.y);
+
+
+		
+
+		*/
+
+		//draw.        
+		app.clear(Color::White);
+		app.draw(dinoArr[index]);
+		app.draw(spike);
+		app.display();
+
+
+	//CursorSettings();
+		}
+	return 0;
+}
+
+/*
+void CursorSettings()
+{
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+	cursorInfo.dwSize = 1;
+	cursorInfo.bVisible = false;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
+*/
